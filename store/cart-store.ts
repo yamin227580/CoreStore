b311822d106368b7547483a44c351f2c2c4a37d0
Yamin <yaminnyein227580@gmail.com>
2024-12-14 12:01:05 +0630

    persisting_the_state/store

--- store/cart-store.ts
+++ store/cart-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export type Variant = {
   variantId: number;
@@ -16,56 +17,65 @@ export type CartType = {
   addToCart: (item: CartItem) => void;
   removeFromCart: (item: CartItem) => void;
 };
-export const useCartStore = create<CartType>((set) => ({
-  cart: [],
-  addToCart: (item) =>
-    set((state) => {
-      const existingItem = state.cart.find(
-        (cItem) => cItem.variant.variantId === item.variant.variantId
-      );
-      if (existingItem) {
-        const updatedCart = state.cart.map((cItem) => {
-          if (cItem.variant.variantId === item.variant.variantId) {
+export const useCartStore = create(
+  persist<CartType>(
+    (set) => ({
+      cart: [],
+      addToCart: (item) =>
+        set((state) => {
+          const existingItem = state.cart.find(
+            (cItem) => cItem.variant.variantId === item.variant.variantId
+          );
+          if (existingItem) {
+            const updatedCart = state.cart.map((cItem) => {
+              if (cItem.variant.variantId === item.variant.variantId) {
+                return {
+                  ...cItem,
+                  variant: {
+                    ...cItem.variant,
+                    quantity: cItem.variant.quantity + item.variant.quantity,
+                  },
+                };
+              }
+              return cItem;
+            });
+            return { cart: updatedCart };
+          } else {
             return {
-              ...cItem,
-              variant: {
-                ...cItem.variant,
-                quantity: cItem.variant.quantity + item.variant.quantity,
-              },
+              cart: [
+                ...state.cart,
+                {
+                  ...item,
+                  variant: {
+                    variantId: item.variant.variantId,
+                    quantity: item.variant.quantity,
+                  },
+                },
+              ],
             };
           }
-          return cItem;
-        });
-        return { cart: updatedCart };
-      } else {
-        return {
-          cart: [
-            ...state.cart,
-            {
-              ...item,
-              variant: {
-                variantId: item.variant.variantId,
-                quantity: item.variant.quantity,
-              },
-            },
-          ],
-        };
-      }
-    }),
-  removeFromCart: (item) =>
-    set((state) => {
-      const updatedCart = state.cart.map((cItem) => {
-        if (cItem.variant.variantId === item.variant.variantId) {
+        }),
+      removeFromCart: (item) =>
+        set((state) => {
+          const updatedCart = state.cart.map((cItem) => {
+            if (cItem.variant.variantId === item.variant.variantId) {
+              return {
+                ...cItem,
+                variant: {
+                  ...cItem.variant,
+                  quantity: cItem.variant.quantity - 1,
+                },
+              };
+            }
+            return cItem;
+          });
           return {
-            ...cItem,
-            variant: {
-              ...cItem.variant,
-              quantity: cItem.variant.quantity - 1,
-            },
+            cart: updatedCart.filter((item) => item.variant.quantity > 0),
           };
-        }
-        return cItem;
-      });
-      return { cart: updatedCart.filter((item) => item.variant.quantity > 0) };
+        }),
     }),
-}));
+    {
+      name: "cart-storage",
+    }
+  )
+);
